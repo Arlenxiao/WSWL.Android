9908b8cef530370b152c50a7d9114c3ee305cf62
arlenxiao <yuqicook@163.com>
2015-05-16 11:56:38 +0800

    基本完成主页UI功能

--- Wswl.MUI/HomeActivity.cs
+++ Wswl.MUI/HomeActivity.cs
@@ -22,6 +22,49 @@ namespace Wswl.MUI
             // Create your application here
 
             SetContentView(Resource.Layout.Home);
+
+            //初始化事件
+            InitEvent();
+        }
+
+        /// <summary>初始化事件</summary>
+        private void InitEvent()
+        {
+            //网关菜单
+            FindViewById<ImageView>(Resource.Id.home_menu_gateway).Click += (s, e) => { Toast.MakeText(this, "网关菜单", ToastLength.Short).Show(); };
+
+            //报警提示
+            FindViewById<TextView>(Resource.Id.lbl_home_alarm).Click += (s, e) => { Toast.MakeText(this, "报警提示--36", ToastLength.Long).Show(); };
+
+            //设置按钮
+            FindViewById<ImageView>(Resource.Id.img_home_icon_setting).Click += (s, e) => { Toast.MakeText(this, "设置管理", ToastLength.Short).Show(); };
+
+            //报警信息
+            FindViewById<TextView>(Resource.Id.lbl_home_warning).Click += (s, e) => { Toast.MakeText(this, "报警信息--喝茶室门窗磁报警", ToastLength.Short).Show(); };
+
+            //在线设备统计
+            FindViewById<Button>(Resource.Id.btn_home_online_devices).Click += (s, e) => { Toast.MakeText(this, "当前在线设备:7/21", ToastLength.Short).Show(); };
+
+            //在线用户统计
+            FindViewById<Button>(Resource.Id.btn_home_online_user).Click += (s, e) => { Toast.MakeText(this, "当前在线设备用户:1/6", ToastLength.Short).Show(); };
+
+            //W0103
+            FindViewById<Button>(Resource.Id.btn_home_dev_w0103_01).Click += (s, e) => { Toast.MakeText(this, "网关W0103,暂时还没有做具体界面,等待做好跳转对应控制界面!", ToastLength.Short).Show(); };
+
+            //茶室开关
+            FindViewById<Button>(Resource.Id.btn_home_dev_k0203_01).Click += (s, e) => { Toast.MakeText(this, "茶室开关,暂时还没有做具体界面,等待做好跳转对应控制界面!", ToastLength.Short).Show(); };
+
+            //大厅红外
+            FindViewById<Button>(Resource.Id.btn_home_dev_A0541_01).Click += (s, e) => { Toast.MakeText(this, "大厅红外,暂时还没有做具体界面,等待做好跳转对应控制界面!", ToastLength.Short).Show(); };
+
+            //办公室开关
+            FindViewById<Button>(Resource.Id.btn_home_dev_k0203_02).Click += (s, e) => { Toast.MakeText(this, "办公室开关,暂时还没有做具体界面,等待做好跳转对应控制界面!", ToastLength.Short).Show(); };
+
+            //车间红外
+            FindViewById<Button>(Resource.Id.btn_home_dev_A0541_02).Click += (s, e) => { Toast.MakeText(this, "车间红外,暂时还没有做具体界面,等待做好跳转对应控制界面!", ToastLength.Short).Show(); };
+
+            //茶室门窗磁
+            FindViewById<Button>(Resource.Id.btn_home_dev_A0501_01).Click += (s, e) => { Toast.MakeText(this, "茶室门窗磁,暂时还没有做具体界面,等待做好跳转对应控制界面!", ToastLength.Short).Show(); };
         }
     }
 }
--- Wswl.MUI/Resources/Resource.Designer.cs
+++ Wswl.MUI/Resources/Resource.Designer.cs
@@ -48,37 +48,58 @@ namespace Wswl.MUI
 			public const int bg = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Icon = 2130837505;
+			public const int header_br = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int icon_devicesl_white = 2130837506;
+			public const int home_alarm = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int icon_home_white = 2130837507;
+			public const int Icon = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int icon_more_white = 2130837508;
+			public const int icon_devicesl_white = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int icon_news_white = 2130837509;
+			public const int icon_home_white = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int logo = 2130837510;
+			public const int icon_menu_white = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int selectedTab = 2130837511;
+			public const int icon_more_white = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int tab_bg = 2130837512;
+			public const int icon_news_white = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int tab_icon_home = 2130837513;
+			public const int icon_setting = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int tab_state = 2130837514;
+			public const int icon_user = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int unselectedTab = 2130837515;
+			public const int icon_warning = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int image_user = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int logo = 2130837517;
+			
+			// aapt resource value: 0x7f02000e
+			public const int selectedTab = 2130837518;
+			
+			// aapt resource value: 0x7f02000f
+			public const int tab_bg = 2130837519;
+			
+			// aapt resource value: 0x7f020010
+			public const int tab_icon_home = 2130837520;
+			
+			// aapt resource value: 0x7f020011
+			public const int tab_state = 2130837521;
+			
+			// aapt resource value: 0x7f020012
+			public const int unselectedTab = 2130837522;
 			
 			static Drawable()
 			{
@@ -99,65 +120,125 @@ namespace Wswl.MUI
 			// aapt resource value: 0x7f060000
 			public const int app_message_image = 2131099648;
 			
-			// aapt resource value: 0x7f06000c
-			public const int btn_login_link = 2131099660;
+			// aapt resource value: 0x7f06001a
+			public const int btn_home_dev_A0501_01 = 2131099674;
 			
-			// aapt resource value: 0x7f060008
-			public const int btn_login_login = 2131099656;
+			// aapt resource value: 0x7f060017
+			public const int btn_home_dev_A0541_01 = 2131099671;
 			
-			// aapt resource value: 0x7f06000a
-			public const int btn_login_register = 2131099658;
+			// aapt resource value: 0x7f060019
+			public const int btn_home_dev_A0541_02 = 2131099673;
 			
-			// aapt resource value: 0x7f060012
-			public const int btn_reg_back = 2131099666;
+			// aapt resource value: 0x7f060016
+			public const int btn_home_dev_k0203_01 = 2131099670;
 			
-			// aapt resource value: 0x7f060011
-			public const int btn_reg_register = 2131099665;
-			
-			// aapt resource value: 0x7f060002
-			public const int imageView1 = 2131099650;
-			
-			// aapt resource value: 0x7f060005
-			public const int img_login_logo = 2131099653;
-			
-			// aapt resource value: 0x7f06000d
-			public const int lbl_reg_title = 2131099661;
-			
-			// aapt resource value: 0x7f060004
-			public const int linearLayout1 = 2131099652;
-			
-			// aapt resource value: 0x7f060009
-			public const int linearLayout2 = 2131099657;
-			
-			// aapt resource value: 0x7f06000b
-			public const int linearLayout3 = 2131099659;
-			
-			// aapt resource value: 0x7f060014
-			public const int tabIcon = 2131099668;
-			
-			// aapt resource value: 0x7f060013
-			public const int tabLayout = 2131099667;
+			// aapt resource value: 0x7f060018
+			public const int btn_home_dev_k0203_02 = 2131099672;
 			
 			// aapt resource value: 0x7f060015
-			public const int tabText = 2131099669;
-			
-			// aapt resource value: 0x7f060003
-			public const int textView1 = 2131099651;
-			
-			// aapt resource value: 0x7f060007
-			public const int txt_login_password = 2131099655;
-			
-			// aapt resource value: 0x7f060006
-			public const int txt_login_userName = 2131099654;
+			public const int btn_home_dev_w0103_01 = 2131099669;
 			
 			// aapt resource value: 0x7f060010
-			public const int txt_reg_confirmPassword = 2131099664;
+			public const int btn_home_online_devices = 2131099664;
 			
-			// aapt resource value: 0x7f06000f
-			public const int txt_reg_password = 2131099663;
+			// aapt resource value: 0x7f060011
+			public const int btn_home_online_user = 2131099665;
+			
+			// aapt resource value: 0x7f060020
+			public const int btn_login_link = 2131099680;
+			
+			// aapt resource value: 0x7f06001e
+			public const int btn_login_login = 2131099678;
+			
+			// aapt resource value: 0x7f06001f
+			public const int btn_login_register = 2131099679;
+			
+			// aapt resource value: 0x7f060026
+			public const int btn_reg_back = 2131099686;
+			
+			// aapt resource value: 0x7f060025
+			public const int btn_reg_register = 2131099685;
+			
+			// aapt resource value: 0x7f060005
+			public const int home_image_br = 2131099653;
+			
+			// aapt resource value: 0x7f060002
+			public const int home_menu_gateway = 2131099650;
+			
+			// aapt resource value: 0x7f06000b
+			public const int img_home_icon_setting = 2131099659;
+			
+			// aapt resource value: 0x7f060009
+			public const int img_home_icon_user = 2131099657;
+			
+			// aapt resource value: 0x7f06000d
+			public const int img_home_icon_warning = 2131099661;
+			
+			// aapt resource value: 0x7f060006
+			public const int img_home_user = 2131099654;
+			
+			// aapt resource value: 0x7f06001b
+			public const int img_login_logo = 2131099675;
+			
+			// aapt resource value: 0x7f060004
+			public const int lbl_home_alarm = 2131099652;
+			
+			// aapt resource value: 0x7f060013
+			public const int lbl_home_devices = 2131099667;
+			
+			// aapt resource value: 0x7f060003
+			public const int lbl_home_gateway = 2131099651;
+			
+			// aapt resource value: 0x7f06000a
+			public const int lbl_home_userName = 2131099658;
 			
 			// aapt resource value: 0x7f06000e
-			public const int txt_reg_userName = 2131099662;
+			public const int lbl_home_warning = 2131099662;
+			
+			// aapt resource value: 0x7f060021
+			public const int lbl_reg_title = 2131099681;
+			
+			// aapt resource value: 0x7f060007
+			public const int linearLayout1 = 2131099655;
+			
+			// aapt resource value: 0x7f060008
+			public const int linearLayout2 = 2131099656;
+			
+			// aapt resource value: 0x7f06000c
+			public const int linearLayout3 = 2131099660;
+			
+			// aapt resource value: 0x7f06000f
+			public const int linearLayout4 = 2131099663;
+			
+			// aapt resource value: 0x7f060012
+			public const int linearLayout5 = 2131099666;
+			
+			// aapt resource value: 0x7f060014
+			public const int linearLayout6 = 2131099668;
+			
+			// aapt resource value: 0x7f060028
+			public const int tabIcon = 2131099688;
+			
+			// aapt resource value: 0x7f060027
+			public const int tabLayout = 2131099687;
+			
+			// aapt resource value: 0x7f060029
+			public const int tabText = 2131099689;
+			
+			// aapt resource value: 0x7f06001d
+			public const int txt_login_password = 2131099677;
+			
+			// aapt resource value: 0x7f06001c
+			public const int txt_login_userName = 2131099676;
+			
+			// aapt resource value: 0x7f060024
+			public const int txt_reg_confirmPassword = 2131099684;
+			
+			// aapt resource value: 0x7f060023
+			public const int txt_reg_password = 2131099683;
+			
+			// aapt resource value: 0x7f060022
+			public const int txt_reg_userName = 2131099682;
 			
 			static Id()
 			{
